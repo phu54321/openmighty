601074d5d1872a578528f15fc07ff93936b0abcf
phu54321 <phu54321@naver.com>
2017-01-22 23:12:04 +0900

    real fix

--- src/clientjs/game/cmdBidding.js
+++ src/clientjs/game/cmdBidding.js
@@ -22,8 +22,8 @@ module.exports = function (cmdTranslatorMap) {
         game.president = -1;
         game.remainingBidder = 5;
         game.lastBidder = null;
-        game.lastBidCount = null;
-        game.lastBidShape = null;
+        game.bidCount = null;
+        game.bidShape = null;
 
         room.playing = true;
         room.viewRoom();
