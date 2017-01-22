e8f1e65bd139bed63406680e48616369d414f984
phu54321 <phu54321@naver.com>
2017-01-22 11:58:15 +0900

    MockSocket connect&disconnect test

--- src/tests/test_reconnect.js
+++ src/tests/test_reconnect.js
@@ -4,41 +4,72 @@
 
 "use strict";
 
+const assert = require('assert');
+
+
+////
+
+
 function MockSocket() {
     this.evHandler = {};
+    this.disconnected = false;
 }
 
 MockSocket.prototype.on = function (event, eventHandler) {
-    this.evHandler[event] = eventHandler;
+    if(!this.evHandler[event]) {
+        this.evHandler[event] = [];
+    }
+    this.evHandler[event].push(eventHandler);
 };
 
 MockSocket.prototype.emit = function (event, data) {
-    console.log('[MockSocket Out]', event, data);
+    assert(!this.disconnected);
+    // console.log('[MockSocket Out]', event, data);
 };
 
-MockSocket.prototype.in = function (event, data) {
-    console.log('[MockSocket In]', event, data);
-    process.nextTick(() => {
-        this.evHandler[event](data);
-    });
+MockSocket.prototype.msgin = function (event, data) {
+    // console.log('[MockSocket In]', event, data);
+    assert(!this.disconnected);
+
+    if(this.evHandler[event]) {
+        this.evHandler[event].forEach((evHandler) => {
+            evHandler(data);
+        });
+    }
+    if(event == 'disconnect') {
+        this.disconnected = true;
+    }
 };
 
 MockSocket.prototype.disconnect = function () {
-    this.emit('disconnect');
+    this.disconnected = true;
 };
 
 
 
 ///////////////
 
-suite('Reconnectable socket');
+describe('MockSocket', function() {
+    describe('#connect()', function() {
+        it('should initiate new connection', function(done) {
+            const socket = new MockSocket();
+            socket.on('cmd', console.log);
+            socket.on('async', (data) => {
+                assert(data == 'test');
+                done();
+            });
+            socket.msgin('async', 'test');
+        });
+    });
 
-test('MockSocket test', function() {
-    const socket = new MockSocket();
-    socket.on('cmd', console.log);
-    socket.emit('test');
-    socket.in('async', 'test');
-    socket.on('async', (data) => {
-        console.log(data);
-    })
+    describe('#disconnect()', function() {
+        it('should cut down any transmission', function(done) {
+            const socket = new MockSocket();
+            socket.disconnect();
+            assert.throws(function() {
+                socket.msgin('async', 'test');
+            }, Error, 'Connection not cut down');
+            done();
+        });
+    });
 });
