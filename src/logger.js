9573971a5caadb0478e6c5d080238d7ccaa144b0
phu54321 <phu54321@naver.com>
2017-02-11 21:18:19 +0900

    better date formatting

--- src/logger.js
+++ src/logger.js
@@ -9,7 +9,16 @@ require('date-utils');
 const logger = new (winston.Logger)({
     transports: [
         new (winston.transports.Console)({
-            timestamp: () => new Date().toString(),
+            timestamp: () => {
+                const currentTime = new Date();
+                const yyyy = currentTime.getFullYear();
+                const mm = currentTime.getMonth() < 9 ? "0" + (currentTime.getMonth() + 1) : (currentTime.getMonth() + 1); // getMonth() is zero-based
+                const dd  = currentTime.getDate() < 10 ? "0" + currentTime.getDate() : currentTime.getDate();
+                const hh = currentTime.getHours() < 10 ? "0" + currentTime.getHours() : currentTime.getHours();
+                const min = currentTime.getMinutes() < 10 ? "0" + currentTime.getMinutes() : currentTime.getMinutes();
+                const ss = currentTime.getSeconds() < 10 ? "0" + currentTime.getSeconds() : currentTime.getSeconds();
+                return `${yyyy}-${mm}-${dd} ${hh}:${mm}:${ss}`;
+            },
             colorize: true,
             level: 'debug'
         })
