83c922699bc5f592d80361d69d26a84894b28e4d
phu54321 <phu54321@naver.com>
2017-02-15 17:47:22 +0900

    some ui?

--- src/routes/game.js
+++ src/routes/game.js
@@ -8,6 +8,8 @@ const express = require('express');
 const users = require('./users');
 const gamelog = require('../models/gamelog');
 const crypto = require('crypto');
+const _ = require('lodash');
+
 
 let router = express.Router();
 module.exports = router;
@@ -23,6 +25,14 @@ router.get('/:room', users.checkLogon, (req, res) => {
     res.render('room');
 });
 
+
+// 카드의 번호를 변환한다
+const numStrList = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+function getCardNumString(num) {
+    if(num === 0) return "";
+    return numStrList[num - 2];
+}
+
 // Room
 router.get('/log/:gameID', users.checkLogon, (req, res, next) => {
     const gameID = req.params.gameID | 0;
@@ -31,25 +41,30 @@ router.get('/log/:gameID', users.checkLogon, (req, res, next) => {
         if(err) return next(err);
 
         let gusers = [];
-        const pbidding = [[], [], [], [], []];  // 각 플레이어가 했던 공약들
+        const pbiddings = [[], [], [], [], []];  // 각 플레이어가 했던 공약들
         const bidding = [];  // 공약 수정 등을 이유로 여러번 공약이 바뀔 수 있으므로 배열로 관리
         const tricks = [];
-        const currentTrick = [];
+        let currentTrick = [];
         let president = null;
         const friend = {};
 
+        // 뷰랑 모델을 분리하기가 어려워서 여기서는 그냥 template를 통해 html을 동적 생성합니다.
+        // TODO: 뷰로 관련 로직을 옮긴다.
+
+        const outputHtmls = [];
+
+        // Simple log parser
         entry.gameLog.forEach(log => {
             if(log.type == 'gusers') {
-                gusers = log.users.map(user => user.username);
+                gusers = log.users;
             }
             else if(log.type == 'pbinfo') {
-                pbidding[log.bidder].push({
+                pbiddings[log.bidder].push({
                     bidShape: log.bidShape,
                     bidCount: log.bidCount
                 });
             }
             else if(log.type == 'binfo') {
-                // { type: 'binfo', president: 3, shape: 'diamond', num: 13 },
                 president = log.president;
                 bidding.push({
                     bidShape: log.shape,
@@ -68,19 +83,25 @@ router.get('/log/:gameID', users.checkLogon, (req, res, next) => {
                     cards: currentTrick,
                     winner: log.winner
                 });
+                currentTrick = [];
             }
         });
 
         console.log(
             gusers,
-            pbidding,
+            pbiddings,
             bidding,
             friend,
             tricks
         );
         res.render('gamelog', {
             gameID: req.params.gameID,
-            gameLog: JSON.stringify(entry.gameLog, null, 4)
+            gameLog: JSON.stringify(entry.gameLog, null, 4),
+            gusers: gusers,
+            pbiddings: pbiddings,
+            bidding: bidding,
+            friend: friend,
+            tricks: tricks
         });
     });
 });
