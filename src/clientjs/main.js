b293ca8b5160c9e8135f570a815f5445e8f57b7d
박현우 <phu54321>
2019-09-14 17:26:06 +0900

    fix: ie block - not well supported

--- src/clientjs/main.js
+++ src/clientjs/main.js
@@ -4,18 +4,30 @@
 
 "use strict";
 
-require('./formIssuer');
-require('./roomjoin');
-require('./game/conn');
-require('./logViewer');
-
 function isKakaoTalkBrowser () {
     return (navigator.userAgent || '').toUpperCase().indexOf('KAKAOTALK') != -1;
 }
 
-if (isKakaoTalkBrowser()) {
-    Materialize.toast("카카오톡 브라우저에서 플레이하던 중 채팅으로 되돌아가면 게임에서 나가질 수 있습니다. 새로운 브라우저 창에서 게임을 실행해주세요.", 15000);
+function isIE () {
+    var agent = navigator.userAgent.toLowerCase();
+    return (navigator.appName == 'Netscape' && agent.indexOf('trident') != -1) || (agent.indexOf("msie") != -1);
 }
+
+if (isIE()) {
+    alert("Internet explorer는 지원되지 않습니다. 다른 브라우저를 써주세요.");
+    location.href = 'about:blank;';
+    throw new Error('fuck');
+}
+
+if (isKakaoTalkBrowser()) {
+    alert("카카오톡 브라우저에서 플레이하던 중 채팅으로 되돌아가면 게임에서 나가질 수 있습니다. 새로운 브라우저 창에서 게임을 실행해주세요.", 15000);
+}
+
+
+require('./formIssuer');
+require('./roomjoin');
+require('./game/conn');
+require('./logViewer');
 
 // Toast should disappear on click
 $(document).on('click', '#toast-container .toast', function() {
